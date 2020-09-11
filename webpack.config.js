6f1f451d05c0033bc9c4a726812c9fa4a9bfe39e
Alexander Wu <alexander.wu7@gmail.com>
2020-09-11 12:06:37 -0700

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,47 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: "babel-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true,
-  },
-};
+module.exports = (env) => {
+  const isProduction = env === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
+    },
+    plugins: [new MiniCssExtractPlugin({ filename: 'styles.css' })],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+    },
+  };
+}
