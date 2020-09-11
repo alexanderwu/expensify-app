eacf1a867833fbe31fabb5e2f7f631a0086b69b9
Alexander Wu <alexander.wu7@gmail.com>
2020-09-11 14:18:57 -0700

    Setup devDependencies and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = (env) => {
   return {
     entry: './src/index.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js',
     },
     plugins: [new MiniCssExtractPlugin({ filename: 'styles.css' })],
@@ -42,6 +42,7 @@ module.exports = (env) => {
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
+      publicPath: '/dist/'
     },
   };
 }
