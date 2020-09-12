05ca37295dae8456d76c0f5fab98c48044874c9d
Alexander Wu <alexander.wu7@gmail.com>
2020-09-11 17:15:11 -0700

    Add ExpensesSummary to expenses dashboard page

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import selectExpensesTotal from '../selectors/expenses-total';
+import selectExpenses from '../selectors/expenses';
 import numeral from 'numeral';
 
 export class ExpensesSummary extends React.Component {
@@ -12,14 +13,18 @@ export class ExpensesSummary extends React.Component {
   render() {
     return (
       <div>
-        {this.props.expenses.length > 0 && <p>{this.printExpenses()}</p>}
+        {this.props.expenses.length > 0 && <h2>{this.printExpenses()}</h2>}
       </div>
     );
   }
 };
 
-const mapStateToProps = (state) => ({
-  expenses: state.expenses,
-});
+const mapStateToProps = (state) => {
+  const visibleExpenses = selectExpenses(state.expenses, state.filters);
+
+  return {
+    expenses: visibleExpenses,
+  };
+};
 
 export default connect(mapStateToProps)(ExpensesSummary);
