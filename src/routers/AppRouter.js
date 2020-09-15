edea6918105fd8937a76f1ffe19cf92f60e8ce6f
Alexander Wu <alexander.wu7@gmail.com>
2020-09-15 15:35:33 -0700

    Add private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,29 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ExpenseDashboardPage from "../components/ExpenseDashboardPage";
-import AddExpensePage from "../components/AddExpensePage";
-import EditExpensePage from "../components/EditExpensePage";
-import HelpPage from "../components/HelpPage";
-import NotFoundPage from "../components/NotFoundPage";
-import Header from "../components/Header";
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import LoginPage from '../components/LoginPage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
+import PrivateRoute from './PrivateRoute';
+
+export const history = createBrowserHistory();
 
 const AppRouter = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <div>
-      <Header />
       <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true}></Route>
-        <Route path="/create" component={AddExpensePage} exact={true}></Route>
-        <Route path="/edit/:id" component={EditExpensePage} exact={true}></Route>
-        <Route path="/help" component={HelpPage} exact={true}></Route>
+        <Route path='/' component={LoginPage} exact={true}></Route>
+        <PrivateRoute path='/dashboard' component={ExpenseDashboardPage} exact={true}></PrivateRoute>
+        <PrivateRoute path='/create' component={AddExpensePage} exact={true}></PrivateRoute>
+        <PrivateRoute path='/edit/:id' component={EditExpensePage} exact={true}></PrivateRoute>
+        <Route path='/help' component={HelpPage} exact={true}></Route>
         <Route component={NotFoundPage}></Route>
       </Switch>
     </div>
-  </BrowserRouter>
+  </Router>
 );
 
 export default AppRouter;
--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-// import toJSON from 'enzyme-to-json';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
 test('should render Header correctly', () => {
-  const wrapper = shallow(<Header />);
-  // expect(wrapper.find('h1').text()).toBe('Expensify');
+  const wrapper = shallow(<Header startLogout={() => { }} />);
   expect(wrapper).toMatchSnapshot();
-})
+});
 
-// import ReactShallowRender from 'react-test-renderer/shallow';
-
-// test('should render Header correctly', () => {
-//   const wrapper = shallow(<Header />);
-//   const renderer = new ReactShallowRender();
-//   renderer.render(<Header />);
-//   expect(renderer.getRenderOutput()).toMatchSnapshot()
-// });
+test('should call startLogout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
+});
